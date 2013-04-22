fd64522bb12afd183a86e0746aea8e3ec5e8cfb6
benn0rDE <benjaminhintz.de@gmail.com>
2013-04-22 16:04:07 +0200

    collision and intersection project

---
+++ CollisionIntersection/src/de/hsbremen/powerwall/collisionintersection/CollisionIntersection.java
@@ -0,0 +1,23 @@
+package de.hsbremen.powerwall.collisionintersection;
+
+import com.jme3.app.SimpleApplication;
+
+public class CollisionIntersection extends SimpleApplication {
+
+	@Override
+	public void simpleInitApp() {
+
+	}
+
+	@Override
+	public void simpleUpdate(float tpf) {
+
+	}
+
+	public static void main(String[] args) {
+		CollisionIntersection app = new CollisionIntersection();
+		
+		app.start();
+	}
+
+}
